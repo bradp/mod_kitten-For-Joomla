023ab4dab73707d920c8df679fd45563b70e8442
Brad Parbs <Brad@bradparbs.com>
2012-07-24 13:04:46 -0500

    Adding user defined sizes.

--- mod_kitten.php
+++ mod_kitten.php
@@ -1,7 +1,7 @@
 <?php
 defined( '_JEXEC' ) or die;
 
-$width = rand(200,300);
-$height = rand(200,300);
+$width = $params->get('width');
+$height = $params->get('height');
 
 echo "<img src='http://placekitten.com/{$width}/{$height}' alt='Kittens!' width='{$width}' height='{$height}' />";
