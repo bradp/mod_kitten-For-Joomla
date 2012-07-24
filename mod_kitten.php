bb00cd6ad1f05fa10f8e70d39b32dc37cd22e84b
Brad Parbs <Brad@bradparbs.com>
2012-07-24 15:25:12 -0500

    Ties in placehold.it to use boring gray placeholders with optional text. Also randomizes the kitten image it loads, to allow for different pictures every time.

--- mod_kitten.php
+++ mod_kitten.php
@@ -3,5 +3,25 @@ defined( '_JEXEC' ) or die;
 
 $width = $params->get('width');
 $height = $params->get('height');
+$type = $params->get('type');
 
-echo "<img src='http://placekitten.com/{$width}/{$height}' alt='Kittens!' width='{$width}' height='{$height}' />";
+if($type == 'kitten'){
+
+  $widthDiffPicture = $width + rand(0,20);
+	$heightDiffPicture = $height + rand(0,20);
+	$base = "http://placekitten.com";
+	$seperator = "/";
+	$text = "";
+
+}else{
+
+	$widthDiffPicture = $width;
+	$heightDiffPicture = $height;
+	$base = "http://placehold.it";
+	$seperator = "x";
+	$text = '&text=' . $params->get('text');
+
+}
+
+
+echo "<img src='{$base}/{$widthDiffPicture}{$seperator}{$heightDiffPicture}{$text}' alt='Placeholder' width='{$width}' height='{$height}' />";
